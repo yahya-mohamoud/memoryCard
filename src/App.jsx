a3becb93b5d0070c6f13d26497d3dcf6d11ed485
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-05-19 12:03:07 +0300

    created the images to shuffle after click, added score and best score added css but to be modified latter

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Cards from './components/Cards'
+function App() {
+  const [data, setData] = useState([])
+  const [isClicked, setIsClicked] = useState([])
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+
+  useEffect(() => {
+    try {
+      
+      const fetchData = async () => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=24`)
+        const fetchedData = await res.json()
+        
+        const detailedPoke = await Promise.all(
+          fetchedData.results.map(async (poke) => {
+            const res = await fetch(poke.url)
+            return await res.json()
+          })
+        )
+        setData(detailedPoke)
+      }
+      fetchData()
+    } catch (error) {
+      console.error("the error is: " + error);
+    }
+  }, [])
+
+ function handleClick(index) {
+    // console.log(data)
+     const shuffled = [...data].sort(() => 0.5 - Math.random())
+    
+     if(!isClicked.includes(index)) {
+        setIsClicked(prev => [...prev, index])
+        setScore(score + 1)
+     } else {
+       setIsClicked([])
+       if(score > bestScore) {
+         setBestScore(score)
+        }
+        setScore(0)
+     }
+
+     setData(shuffled)
+     console.log(isClicked);
+     
+  }
+
+  return (
+    <div>
+      <Header />
+      {data && <Cards data={data} onclick={handleClick} score={score} bestScore={bestScore}/>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Cards.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Cards({ data, onclick, score, bestScore }) {
+
+  const cards = data.slice(0, 12).map((card) => {
+    return <div className="card" key={card.name} onClick={() => onclick(card.name)}>
+      <img src={card.sprites.front_default} alt={card.name} />
+    </div>
+  })
+  return (
+    <main>
+      <div className="score">
+        <h2>Score: {score}</h2>
+        <h2>Best Score: {bestScore}</h2>
+      </div>
+      <div className='cards'>
+        {cards}
+      </div>
+    </main>
+  )
+}
